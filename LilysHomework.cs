3708d041efee38675103e7351a9745eb0265ce72
Vitaly Matkovsky <vitaly.matkovsky@gmail.com>
2022-07-20 17:53:04 +0300

    Added implementation of the Lily's Homework problem.

---
+++ LilysHomework.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank
+{
+    internal class LilysHomework
+    {
+        int CalcDiff(List<int> array)
+        {
+            Dictionary<int, int> dic = array.Select((v, i) => new { Value = v, Idx = i }).ToDictionary(v => v.Value, v => v.Idx);
+            int[] arr = array.ToArray();
+            int cnt = 0;
+            for (int i = 0; i < arr.Length; ++i)
+                if (arr[i] != i)
+                {
+                    int i1 = dic[i];    //the index where current element will be moved to
+                    dic[arr[i]] = i1;
+                    arr[i1] = arr[i];
+                    cnt++;
+                }
+            return cnt;
+        }
+
+        public int Do(List<int> arr)
+        {
+            Dictionary<int, int> dic = arr.OrderBy(v => v).Select((v, i) => new { Value = v, Idx = i }).ToDictionary(v => v.Value, v => v.Idx);
+            
+            for(int i = 0; i < arr.Count; ++i)
+                arr[i] = dic[arr[i]];
+            int cnt1 = CalcDiff(arr);
+            arr.Reverse();
+            return Math.Min(cnt1, CalcDiff(arr));
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace HackerRank
+{
+    class Program
+    {
+        static void RunDataCenter()
+        {
+            Console.WriteLine("Running the Data Center task");
+            DataCenter dc = new DataCenter();
+            List<int> bootingPowers = new List<int> { 5, 10, 15, 20 };
+            List<int> processingPowers = new List<int> { 7, 4, 2, 6 };
+            int maxCluster = dc.SearchCluster(19, bootingPowers, processingPowers);
+            Console.WriteLine($"Result = {maxCluster}");
+        }
+
+        static void RunLilysHomework()
+        {
+            Console.WriteLine("Running the Lily's Homework task");
+            LilysHomework lh = new LilysHomework();
+            List<int> array = new List<int> 
+            //                              { 3, 4, 0, 1, 5, 2 };
+            //                              { 5, 4, 0, 1, 3, 2 };
+            //                              { 5, 4, 3, 1, 0, 2 };
+                                          { 5, 1, 3, 2, 4, 0 };
+            //                              { 5, 4, 3, 2, 1, 0 };
+            //                              { 0, 1, 2, 3, 4, 5 };
+            int result = lh.Do(array);
+            Console.WriteLine($"Result = {result}");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose one of the problems:");
+            Console.WriteLine("1 - Data Center");
+            Console.WriteLine("2 - Lily's Homework");
+            Console.Write("> ");
+            ConsoleKeyInfo key = Console.ReadKey();
+            Console.WriteLine();
+            switch(key.KeyChar)
+            {
+                case '1': RunDataCenter(); break;
+                case '2': RunLilysHomework(); break;
+                default: Console.WriteLine("Invalid choice");  break;
+            }
+        }
+    }
+}
